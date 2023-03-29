fb236635abc2e1db64d8532a8a8a4fb788ec8e05
Nick <nickvanbokhorst@hotmail.com>
2023-03-29 15:40:44 +0200

    soong: Whitelist llvm-addr2line (#14)
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -101,6 +101,7 @@ var Configuration = map[string]PathConfig{
 	"jar":     Allowed,
 	"java":    Allowed,
 	"javap":   Allowed,
+	"llvm-addr2line":  Allowed,
 	"locale":  Allowed,
 	"lsof":    Allowed,
 	"ld.lld":  Allowed,
